8eafa97f558f338a7a3f181137f592bbe7af9b91
esterarvilla2 <esterarvillagras@yahoo.es>
2023-12-15 10:44:41 +0100

    Changed password

--- project/settings.py
+++ project/settings.py
@@ -83,18 +83,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-  'default': {
-      'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django',
         'USER': 'esterarvilla',
-        'PASSWORD': os.getenv(DB_PASSWORD),
+        'PASSWORD': 'DjangoIS23',
         'HOST': 'esterarvilla.postgres.database.azure.com',
         'PORT': '5432',
         'OPTIONS': {'sslmode': 'require'}
